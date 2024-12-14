c765327892d24fdb98d12aec50565df25bd6700d
danhelmer <dhelmer25@gmail.com>
2024-12-13 23:53:17 -0500

    added docx capability

--- backend/controller/resumeUploadController.js
+++ backend/controller/resumeUploadController.js
@@ -1,6 +1,7 @@
 const multer = require("multer");
 const upload = require("../config/multerConfig");
 const { extractTextFromPdf } = require("../helper/pdfExtractor");
+const mammoth = require('mammoth');
 
 const resumeUploadHandler = (req, res) => {
   upload.single("resume_file")(req, res, async (err) => {
@@ -30,9 +31,18 @@ const resumeUploadHandler = (req, res) => {
           textContent: extractedText,
         });
       }
+      if (file.mimetype === "application/vnd.openxmlformats-officedocument.wordprocessingml.document") {
+        const result = await mammoth.extractRawText({ buffer: file.buffer });
+        const extractedText = result.value;
+        return res.status(200).json({
+          message: "Resume uploaded successfully.",
+          textContent: extractedText,
+        });
+      }
+
 
       return res.status(200).json({
-        message: "Resume uploaded successfully (no text extraction for docx).",
+        message: "Resume uploaded successfully.",
       });
     } catch (error) {
       return res
