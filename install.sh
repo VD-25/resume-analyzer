c073e38b7ea0261275dab2c414110dd92067ce55
danhelmer <dhelmer25@gmail.com>
2024-12-15 20:44:17 -0500

    edit install script

--- install.sh
+++ install.sh
@@ -1,10 +1,5 @@
 cd ./backend
 npm install
-echo RUNNING BACKEND UNIT TESTS------------------------------------
-npm test
-cd ..frontend
+cd ../frontend
 npm install
-echo RUNNING FRONTEND UNIT TESTS-----------------------------------
-npm test
-cd ../backend
-npm start # starts backend nodejs
+cd ..
